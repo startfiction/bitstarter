a256610edea87399b0e36e40519e500a900979fa
Start Fiction <startfiction@gmail.com>
2013-07-12 22:53:56 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(16);
+buffer = fs.readFileSync('index.html');
+var hello = buffer.toString('utf8', 0, len);
+
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!')
+  response.send(hello)
 });
 
 var port = process.env.PORT || 5000;
